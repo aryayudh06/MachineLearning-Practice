40f0f99612d2e152527bc13008396e4b7769fe43
aryayudh06 <aryayudhakp@gmail.com>
2025-01-19 22:00:27 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   10_svm.ipynb
    	new file:   11_random_forest.ipynb
    	new file:   12_k_fold.ipynb
    	new file:   13_kmeans_tutorial.ipynb
    	new file:   14_naive_bayes_1_titanic_survival_prediction.ipynb
    	new file:   14_naive_bayes_2_email_spam_filter.ipynb
    	new file:   15_grid_search.ipynb
    	new file:   1_linear_regression.ipynb
    	new file:   2_linear_regression_multivariate.ipynb
    	new file:   3_gradient_descent.ipynb
    	new file:   4_save_and_load_model_using_pickle.ipynb
    	new file:   5_one_hot_encoding.ipynb
    	new file:   6_train_test_split.ipynb
    	new file:   7_logistic_regression.ipynb
    	new file:   8_logistic_regression_multiclass.ipynb
    	new file:   9_decision_tree.ipynb
    	new file:   HR_comma_sep.csv
    	new file:   TEST.PY
    	new file:   Test.java
    	new file:   canada_per_capita_income.csv
    	new file:   carprices.csv
    	new file:   hiring.csv
    	new file:   homeprices.csv
    	new file:   plot_iris_dataset.ipynb
    	new file:   test_scores.csv
    	new file:   titanic.csv
    	new file:   try1.ipynb
    	new file:   try10.ipynb
    	new file:   try11.ipynb
    	new file:   try12.ipynb
    	new file:   try13.ipynb
    	new file:   try14.ipynb
    	new file:   try15.ipynb
    	new file:   try2.ipynb
    	new file:   try5.ipynb
    	new file:   try5_v2.ipynb
    	new file:   try6.ipynb
    	new file:   try8.ipynb
    	new file:   try9.ipynb
    	new file:   try_3.ipynb
    	new file:   untitled.ipynb

---
+++ Test.java
@@ -0,0 +1,10 @@
+
+public class Test {
+
+  public static void main(String[] args) {
+    int x = 3600 * 24;
+    int y = x * 30;
+    int z = y * 12;
+    System.out.print(5 * z + 5 * y + x * 5);
+  }
+}
